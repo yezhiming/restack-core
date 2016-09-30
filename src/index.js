d2aedfabfc35a75925da81c3d4ae9ee546ee119f
Justin Yip <justin.yip@foxmail.com>
2016-09-30 10:43:06 +0800

    do not export actions from index

--- src/index.js
+++ src/index.js
@@ -5,5 +5,3 @@ export createReducer from './utils/createReducer'
 export i18n from './i18n'
 
 export App from './app'
-
-export * as actions from './actions'
--- test/actions.spec.js
+++ test/actions.spec.js
@@ -3,14 +3,14 @@ import {showModal, SHOW_MODAL} from '../lib/actions'
 
 describe('provided actions', () => {
 
-  it('can access via restack/actions', () => {
-    console.log(actions)
+  it('can access via restack-core/actions', () => {
     expect(actions).not.toBeNull();
+    expect(actions.SHOW_MODAL).toBe("SHOW_MODAL")
   })
 
   it('can access individual action / action creator via import', () => {
-    expect(showModal).toBeDefined()
     expect(typeof showModal).toBe('function')
     expect(SHOW_MODAL).toBe("SHOW_MODAL")
   })
+
 })
