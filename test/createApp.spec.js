376bad23443837b1a7b62c7b9cefee69cf3ff92d
Justin Yip <justin.yip@foxmail.com>
2016-12-02 14:59:10 +0800

    test case

---
+++ test/createApp.spec.js
@@ -0,0 +1,62 @@
+import createApp from '../src/app'
+
+import i18n from '../src/plugins/i18n-plugin'
+
+import { takeEvery, takeLatest, delay } from 'redux-saga'
+import { take, put, call, fork, select, race } from 'redux-saga/effects'
+
+describe('create app', () => {
+
+  it('can be created', () => {
+
+    const app = createApp({});
+
+    expect(app).not.toBeNull();
+  })
+
+  it('can render on server', () => {
+    const app = createApp({});
+
+    app.render().then(result => {
+      expect(result).not.toBeNull()
+    });
+  })
+
+  it('can define models', () => {
+    const app = createApp({});
+
+    app.model({
+      name: 'userList',
+      initialState: {
+        isFetching: false
+      },
+      reducers: {
+        xxx: function(state, action) {
+          return state;
+        }
+      },
+      sagas: {
+
+        *list() {
+
+          yield put({type: 'userList/xxx'})
+
+          yield update({isFetching: true})
+
+          const { response, error} = yield call(fetch, `http://www.baidu.com`)
+
+          if (response) {
+
+            yield update({isFetching: false, items: response})
+          } else {
+
+            yield update({isFetching: true, error})
+          }
+        }
+      }
+    })
+
+  })
+
+
+})
--- test/model.spec.js
+++ test/model.spec.js
@@ -1,26 +1,26 @@
 import createApp from '../src/app'
 import {UPDATE} from '../src/model'
 
-describe('create app', () => {
-  it('can define reducers', () => {
+// 产生效果的action
+const TAKE_EFFECT_ACTION = 'TAKE_EFFECT_ACTION'
+// 不产生效果的action
+const TAKE_NO_EFFECT_ATION = 'TAKE_NO_EFFECT_ATION'
+// 初始任务名字
+const INITIAL_NAME = "INITIAL_NAME"
+// 修改后的任务名字
+const MODIFIED_NAME = "MODIFIED_NAME"
+// 初始任务状态
+const initialState = {done: false, name: INITIAL_NAME}
 
-    // 产生效果的action
-    const TAKE_EFFECT_ACTION = 'TAKE_EFFECT_ACTION'
-    // 不产生效果的action
-    const TAKE_NO_EFFECT_ATION = 'TAKE_NO_EFFECT_ATION'
-    // 初始任务名字
-    const INITIAL_NAME = "INITIAL_NAME"
-    // 修改后的任务名字
-    const MODIFIED_NAME = "MODIFIED_NAME"
-    // 初始任务状态
-    const initialState = {done: false, name: INITIAL_NAME}
+describe('create app', () => {
+  it('can define reducers as object', () => {
 
     const app = createApp({});
 
     app.model({
       name: "test",
       reducers: {
-        tasks: function(state = initialState, action) {
+        task: function(state = initialState, action) {
           if (action.type === TAKE_EFFECT_ACTION) {
             return {...state, done: true}
           }
@@ -33,23 +33,47 @@ describe('create app', () => {
 
     const { store } = window;
 
-    expect(store.getState().test).toEqual({tasks: initialState})
+    expect(store.getState().test).toEqual({task: initialState})
 
     // no effect
     store.dispatch({type: TAKE_NO_EFFECT_ATION})
-    expect(store.getState().test).toEqual({tasks: initialState})
+    expect(store.getState().test).toEqual({task: initialState})
 
     // take effect
     store.dispatch({type: TAKE_EFFECT_ACTION})
-    expect(store.getState().test).toEqual({tasks: {...initialState, done: true}})
+    expect(store.getState().test).toEqual({task: {...initialState, done: true}})
 
     // update effect
     store.dispatch({
       type: UPDATE,
       namespace: 'test',
-      path: 'tasks',
+      path: 'task',
       updates: {name: MODIFIED_NAME}
     })
-    expect(store.getState().test.tasks.name).toEqual(MODIFIED_NAME)
+    expect(store.getState().test.task.name).toEqual(MODIFIED_NAME)
+  })
+
+  it('can define reducers as function', () => {
+
+    const app = createApp({});
+
+    app.model({
+      name: "test",
+      initialState,
+      reducers: function(state = initialState, action) {
+        if (action.type === TAKE_EFFECT_ACTION) {
+          return {...state, done: true}
+        }
+        return state;
+      }
+    })
+
+    app.create()
+
+    const { store } = window;
+
+    console.log(store.getState())
+
+    expect(store.getState().test).toEqual(initialState)
   })
 })
